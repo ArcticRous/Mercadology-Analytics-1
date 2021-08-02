62a9c54b4f86094bf9e3826a38cb34611098a3e4
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-08-02 15:17:22 -0500

    Modelo de datos de solicitud

---
+++ src/app/models/solicitud.model.ts
@@ -0,0 +1,11 @@
+export class SolicitudModel{
+    cuenta: string;
+    fecha: string;
+    material: string;
+    disenos: number;
+    desDisenos: string;
+    infDisenos: string;
+    urgencia: string;
+    existeMaterial: string;
+    fileMaterial?: any;
+}
