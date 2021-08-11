6e0a6f153bb97ef5185a20fa0831ab530b26ead6
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-08-11 10:31:06 -0500

    Modelo de datos de solicitud agregar campo correo

--- src/app/models/solicitud.model.ts
+++ src/app/models/solicitud.model.ts
@@ -2,11 +2,16 @@ export class SolicitudModel{
     id:string;
     cuenta: string;
     fecha: string;
+    correo: string;
     material: string;
     disenos: number;
     desDisenos: string;
     infDisenos: string;
     urgencia: string;
-    existeMaterial: string;
+    existeMaterial: boolean;
     fileMaterial?: any;
+
+    constructor(){
+        this.existeMaterial = false;
+    }
 }
