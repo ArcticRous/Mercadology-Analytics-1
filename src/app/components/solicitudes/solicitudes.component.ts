63a51ad0ea6c5c41250ea9beb434b7ba1c05d5f2
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-08-24 09:42:18 -0500

    Integracion de la libreía Dropzone para cargar imagenes

--- src/app/components/solicitudes/solicitudes.component.ts
+++ src/app/components/solicitudes/solicitudes.component.ts
@@ -5,6 +5,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { forkJoin } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-solicitudes',
@@ -12,35 +13,37 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./solicitudes.component.css']
 })
 export class SolicitudesComponent implements OnInit {
-  guardaCargaAnterior;
+
   //Para el control del formulario
   solicitudForm: FormGroup;
 
   //Modelo de Solicitud
   solicitudModel: SolicitudModel;
 
-  //Solo lo uso para guardar el reader onload supuesta del PDF pero no lo renderiza
-  fileMaterial: any;
-  //Lo uso en la parte de renderizacion dl PDF pero no se hace esa parte
+  //Lo uso en la parte de renderizacion dl PDF pero no se hace esa parte pero aun asi guarda el archivo
   fileArchivo: any;
 
   //Guarda todos los archivos
   archivos = [];
-  //Guarda los nombres de las imagenes para mostrarlas
-  img = [];
-  //Solo par mostrar un texto fijo en los archivos subidos
-  textoLabel: string = "Seleccionar archivo";
-  //Guarda las url de las imagenes
+
+  //Guarda las URL de las imagenes una vez que se suben al storage
   imgProporcionadas: any[] = [];
 
   //Boleanos para ver si proporciona imagenes o archivos
   booleanFile: boolean = false;
   booleanImg: boolean = false;
 
+  //Para abrir y cerrar la sección drop de imagenes
   abrirCerrar: boolean = false;
+  //Pone la fecha por defecto
   fechaDefecto: string = "";
 
-  constructor(private auth: AuthService, private _snackBar: MatSnackBar) {
+  //Guarda los archivos que se suben con dropone
+  filesDropZone: File[] = [];
+  //Guarda los nombre de los archivos para hacer comparación si los nuevos archivos que se suben son iguales a los existentes
+  nomImg: any[] = [];
+
+  constructor(private auth: AuthService, private _snackBar: MatSnackBar, private toastr: ToastrService) {
     const fecha = new Date();
     const dia = fecha.getDate();
     const mes = (fecha.getMonth() + 1)
@@ -61,27 +64,66 @@ export class SolicitudesComponent implements OnInit {
     this.solicitudForm = new FormGroup({
       cuenta: new FormControl("Cinthya García", [Validators.required]),
       fecha: new FormControl(this.fechaDefecto, [Validators.required]),
-      material: new FormControl(null, [Validators.required]),
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      material: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+      email: new FormControl(null, [Validators.required,  Validators.pattern(/^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/)]),
       numDisenos: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(100)]),
-      desDisenos: new FormControl(null, [Validators.required]),
-      infDisenos: new FormControl(null, [Validators.required]),
+      desDisenos: new FormControl(null, [Validators.required, Validators.minLength(10)]),
+      infDisenos: new FormControl(null, [Validators.required, Validators.minLength(10)]),
       imagenes: new FormControl(null),
       existeMaterial: new FormControl(null, [Validators.required]),
       fileMaterial: new FormControl(null)
     })
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+
+  getErrores(campo:string){
+    return this.solicitudForm.controls[campo].errors && this.solicitudForm.controls[campo].touched;
+  }
+
+  showToastWarning(nombre: string, mensaje: string, titulo: string) {
+    this.toastr.warning(`El archivo "${nombre}" ${mensaje}`, `${titulo}`);
+  }
+
+  onSelect(event) {
+    if (this.filesDropZone.length > 0) {
+
+      for (const elemento of event.addedFiles) {
+
+        if (this.nomImg.includes(elemento.name)) {
+          this.showToastWarning(elemento.name, "ya ha sido adjuntado", 'Archivo repetido!');
+        } else if (elemento.size > 3145728) {
+          this.showToastWarning(elemento.name, "supera el límite permitido de 3MB", 'Archivo muy grande!');
+        } else {
+          this.filesDropZone.push(elemento);
+        }
+      }
+
+    } else {
+      for (const file of event.addedFiles) {
+        if (file.size > 3145728) {
+          this.showToastWarning(file.name, "supera el límite permitido de 3MB", 'Archivo muy grande!');
+        } else {
+          this.filesDropZone.push(file);
+        }
+      }
+
+    }
+
+    this.nomImg = this.filesDropZone.map(({ name }) => name)
+  }
+
+  onRemove(event) {
+    this.filesDropZone.splice(this.filesDropZone.indexOf(event), 1);
+    this.nomImg = this.filesDropZone.map(({ name }) => name)
   }
 
   /**Metodo para ver el cambio que existe en el input y el evento ejecutado guarda los datos del archivo **/
   onfileArchivo(event) {
-    console.log(event);
 
     //Guardamos los datos del archivo
     const file = event.target.files[0];
-    console.log(file.type);
 
     if (file.size <= 3145728) {
       /**Vemos que exista algo en el archivo */
@@ -101,9 +143,7 @@ export class SolicitudesComponent implements OnInit {
           this.fileArchivo = file;
 
         } else {
-          // console.log('Hay un error');
           this.solicitudForm.value.fileMaterial = null;
-          console.log(this.solicitudForm.value.fileMaterial);
 
           Swal.fire({
             title: "Formato de archivo",
@@ -115,67 +155,12 @@ export class SolicitudesComponent implements OnInit {
         // console.log('No entro' + event.target.files + event.target.files.lenght);
       }
     } else {
-      Swal.fire({
-        title: "Archivo demasiado grande",
-        text: "Cargue un archivo menor a 3MB",
-        icon: "warning"
-      })
+      this.showToastWarning(file.name, "pesa mas de 3MB, adjunte uno de menor peso", 'Archivo muy grande!');
     }
-
-  }
-
-  onfileImagenes(event) {
-
-    let archivosTempo = [...event.target.files];
-    console.log(archivosTempo);
-
-
-    for (let i = 0; i < archivosTempo.length; i++) {
-      if (archivosTempo[i].type.includes("image")) {
-        let variable = archivosTempo[i].name;
-        console.log("ess:::  ", archivosTempo[i].type);
-
-
-        if (!this.img.includes(variable)) {
-          this.archivos.push(archivosTempo[i]);
-          this.img.push(variable);
-        }
-      } else {
-        this.openSnackBar("Solo se pueden cargar imágenes")
-      }
-
-    }
-
-    if (this.img.length == 0) {
-      this.textoLabel = `Seleccionar archivo`;
-    } else if (this.img.length == 1) {
-      this.textoLabel = `${this.img.length} archivo seleccionado`;
-    } else {
-      this.textoLabel = `${this.img.length} archivos seleccionados`;
-    }
-
-    this.solicitudForm.value['fileMaterial'] = "";
-    this.solicitudForm.value['imagenes'] = null;
-  }
-
-  eliminarImg(indice: any) {
-    this.solicitudForm.value['imagenes'] = null;
-
-    this.img.splice(indice, 1);
-    this.archivos.splice(indice, 1);
-    this.textoLabel = `${this.archivos.length} archivos seleccionados`;
-
-    // this.solicitudForm.value['fileMaterial'] = this.archivos;
-    if (this.img.length == 0) {
-      this.textoLabel = "Seleccionar archivo";
-    } else if (this.img.length == 1) {
-      this.textoLabel = `${this.img.length} archivo seleccionado`;
-    }
-
   }
 
   guardar() {
-    if (this.solicitudForm.invalid) { return false }
+    if (this.solicitudForm.invalid) {this.solicitudForm.markAllAsTouched(); return; }
 
     let longitudImgs;
 
@@ -186,13 +171,14 @@ export class SolicitudesComponent implements OnInit {
       this.booleanFile = false;
     } else if (this.solicitudForm.value.existeMaterial == "Si") {
       if (!this.solicitudForm.value.fileMaterial) {
-        console.log(this.solicitudForm.value.fileMaterial);
-
+        // console.log(this.solicitudForm.value.fileMaterial);
         Swal.fire({ title: "Falta material", text: "Debe proporcionar material", icon: "warning" });
         return false;
       }
       this.booleanFile = true;
     }
+
+    this.archivos = this.filesDropZone;
 
     if (this.archivos.length) {
       this.booleanImg = true;
@@ -206,9 +192,19 @@ export class SolicitudesComponent implements OnInit {
     }
 
     Swal.fire({
+      icon: 'question',
+      title: `¿Desea enviar la solicitud?`,
+      text: `¿Ha proporcionado el material y los datos correctamente?`,
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Enviar'
+    }).then(resp => {
+      if(!resp.value){ return;}
+    })
+
+    Swal.fire({
       allowOutsideClick: false,
       icon: 'info',
-      text: 'Espere por favor, se estan subiendo los archivos...'
+      text: 'Espere por favor, se esta enviando la información, puede demorar unos minutos por el tamaño de las imágenes...'
     });
     Swal.showLoading();
 
@@ -286,8 +282,10 @@ export class SolicitudesComponent implements OnInit {
           })
           this.imgProporcionadas = [];
           this.archivos = [];
-          this.img = [];
-          this.textoLabel = "Seleccionar archivo";
+          // this.img = [];
+          this.nomImg = [];
+          this.filesDropZone = [];
+          // this.textoLabel = "Seleccionar archivo";
           this.solicitudForm.value['fileMaterial'] = "";
           this.solicitudForm.reset({
             'cuenta': "Cinthya García",
@@ -311,8 +309,10 @@ export class SolicitudesComponent implements OnInit {
         })
         this.imgProporcionadas = [];
         this.archivos = [];
-        this.img = [];
-        this.textoLabel = "Seleccionar archivo";
+        // this.img = [];
+        this.nomImg = []
+        this.filesDropZone = [];
+        // this.textoLabel = "Seleccionar archivo";
         this.solicitudForm.value['fileMaterial'] = "";
         this.solicitudForm.reset({
           'cuenta': "Cinthya García",
@@ -322,7 +322,6 @@ export class SolicitudesComponent implements OnInit {
     }
   }
 
-
   abrirImagenes() {
     this.abrirCerrar = true;
   }
@@ -331,11 +330,58 @@ export class SolicitudesComponent implements OnInit {
     this.abrirCerrar = false;
   }
 
-  openSnackBar(texto: string): void {
-    this._snackBar.open(texto, "Cerrar", {
-      duration: 4000,
-    });
-  }
-
-
 }
+
+
+
+
+
+
+  // onfileImagenes(event) {
+  //   let archivosTempo = [...event.target.files];
+  //   console.log(archivosTempo);
+  //   let archivosError = []
+  //   for (let i = 0; i < archivosTempo.length; i++) {
+  //     if (archivosTempo[i].type.includes("image")) {
+  //       let variable = archivosTempo[i].name;
+  //       console.log("ess:::  ", archivosTempo[i].type);
+  //       if (!this.img.includes(variable)) {
+  //         this.archivos.push(archivosTempo[i]);
+  //         this.img.push(variable);
+  //       }
+  //     } else {
+  //       archivosError.push(archivosTempo[i].name)
+  //     }
+  //   }
+  //   console.log(archivosError);
+  //   for (let i = 0; i < archivosError.length; i++) {
+  //     const element = archivosError[i];
+  //     console.log(element);
+  //   }
+  //   if (this.img.length == 0) {
+  //     this.textoLabel = `Seleccionar archivo`;
+  //   } else if (this.img.length == 1) {
+  //     this.textoLabel = `${this.img.length} archivo seleccionado`;
+  //   } else {
+  //     this.textoLabel = `${this.img.length} archivos seleccionados`;
+  //   }
+
+  //   this.solicitudForm.value['fileMaterial'] = "";
+  //   this.solicitudForm.value['imagenes'] = null;
+  // }
+
+  // eliminarImg(indice: any) {
+  //   this.solicitudForm.value['imagenes'] = null;
+
+  //   this.img.splice(indice, 1);
+  //   this.archivos.splice(indice, 1);
+  //   this.textoLabel = `${this.archivos.length} archivos seleccionados`;
+
+  //   // this.solicitudForm.value['fileMaterial'] = this.archivos;
+  //   if (this.img.length == 0) {
+  //     this.textoLabel = "Seleccionar archivo";
+  //   } else if (this.img.length == 1) {
+  //     this.textoLabel = `${this.img.length} archivo seleccionado`;
+  //   }
+
+  // }
