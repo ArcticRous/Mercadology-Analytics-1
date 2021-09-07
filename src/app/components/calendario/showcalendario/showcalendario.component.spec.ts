8506e5e0eea9438060c51e0fa9cd2b6e27268492
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-09-07 00:05:11 -0500

    Mostrar calendario de eventos

---
+++ src/app/components/calendario/showcalendario/showcalendario.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowcalendarioComponent } from './showcalendario.component';
+
+describe('ShowcalendarioComponent', () => {
+  let component: ShowcalendarioComponent;
+  let fixture: ComponentFixture<ShowcalendarioComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ShowcalendarioComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShowcalendarioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
