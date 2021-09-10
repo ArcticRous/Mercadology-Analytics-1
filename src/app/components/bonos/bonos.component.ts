315256f693336205bc73ec24a612bb36806b1c48
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-09-10 12:21:54 -0500

    Vista todos bonos, opciones eliminar y editar

--- src/app/components/bonos/bonos.component.ts
+++ src/app/components/bonos/bonos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { BonoModel } from '../../models/bono.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-bonos',
@@ -18,6 +19,33 @@ export class BonosComponent implements OnInit {
         console.log(data);
         this.bonos = data;
     });
+  }
+
+  eliminarBono(bono: any){
+    console.log(bono);
+    
+    Swal.fire({
+      title: '¿Está seguro?',
+      text: `Está seguro que desea borrar a ${bono.nombre}`,
+      icon: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then(resp => {
+
+      if (resp.value) {
+        this.AuthService.deleteBono(bono.id).subscribe(next => {
+          console.log(next);
+          Swal.fire({
+            title: 'Se elimino bono',
+            text: `Se elimino correctamente el bono ${bono.nombre}`,
+            icon: 'success'
+          })
+        }, error => {
+          console.log(error);
+        })
+      }
+    })
+
   }
 
 }
