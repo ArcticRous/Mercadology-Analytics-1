94b17dbac3c5243c239b1d9bf56cc7c4483f1f0a
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-09-13 10:27:52 -0500

    Cambios diseño home comunicados Alan

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,6 +4,9 @@ import { ComunicadoModel } from "../../models/comunicado.model";
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+let comun;
+
+declare let $: any;
 
 @Component({
   selector: 'app-home',
@@ -20,7 +23,7 @@ export class HomeComponent implements OnInit {
   durationInSeconds = 2;
   rol: string;
 
-  displayedColumns: string[] = ['comunicado','ids'];
+  displayedColumns: string[] = ['comunicado'];
   dataSource: MatTableDataSource<ComunicadoModel>;
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
@@ -38,12 +41,14 @@ export class HomeComponent implements OnInit {
     this.cargando = true;
     this.Auth.getComun("publico")
       .subscribe(resp => {
+        comun = resp
         this.Comunicado = resp
         this.cargando = false;
         this.dataSource = new MatTableDataSource(this.Comunicado);
-        this.paginator._intl.itemsPerPageLabel = "comunicados por página";
+        this.paginator._intl.itemsPerPageLabel = "";
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
+
       });
   }//Termina ngOnInit
 
@@ -64,7 +69,5 @@ export class HomeComponent implements OnInit {
       this.dataSource.paginator.firstPage();
     }
   }
-
-
 
 }
