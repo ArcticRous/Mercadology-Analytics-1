7f80b82c358a1890f326842af5da648468cb5558
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-09-14 23:13:02 -0500

    Se agrega la productividad al usuario que desee

--- src/app/components/productividad/productividad.component.ts
+++ src/app/components/productividad/productividad.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ProductividadModel } from '../../models/productividad.model';
 import Swal from 'sweetalert2';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-productividad',
@@ -12,61 +13,104 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./productividad.component.css']
 })
 export class ProductividadComponent implements OnInit {
-  temporalPro: Object = {
-    'usuario': "",
-    'productividad': "",
-    'mes': ""
+  cargando: boolean = true
+  selectOptions: any;
 
-  };
-  productividad: ProductividadModel = new ProductividadModel();
+  productividad: ProductividadModel = new ProductividadModel;
+  productividadForm: FormGroup;
 
-  constructor( private AuthService: AuthService,
-              private route: ActivatedRoute) { }
+  date = new Date();
+  anio = 0
+  anioArray = []
+
+  constructor(private AuthService: AuthService,
+    private route: ActivatedRoute, private fb: FormBuilder) {
+    if (this.date.getMonth() == 0) {
+      this.anioArray.push(this.date.getFullYear() - 1);
+      this.anioArray.push(this.date.getFullYear());
+    }
+    this.anio = this.date.getFullYear();
+  }
 
   ngOnInit(): void {
-  const id = this.route.snapshot.paramMap.get('id');
-  if ( id !== 'add'){
-    this.AuthService.getProID(id)
-    .subscribe( (resp: ProductividadModel) =>{
 
-          this.temporalPro['titulo'] = resp.usuario;
-          this.temporalPro['descripcion'] = resp.productividad;
-          this.temporalPro['fecha'] = resp.mes;
-          
-      this.productividad = resp;
-      this.productividad.id = id;
+    const id = this.route.snapshot.paramMap.get('id');
+
+    this.AuthService.getUsuarios().subscribe(next => {
+      this.selectOptions = next
+    }, error => {
+      console.log(error);
+    }, () => {
+        this.inicializar()
     })
+
   }
-    
+
+  inicializar() {
+    this.productividadForm = this.fb.group({
+      usuario: [this.selectOptions[0].id, [Validators.required]],
+      productividad: [0, [Validators.required, Validators.min(0), Validators.max(100)]],
+      mes: ["0", [Validators.required]],
+      anio: [this.anio, [Validators.required]]
+    });
+    this.cargando = false;
   }
-  guardar(form: NgForm){
-    
-    console.log(form);
-    console.log(this.productividad);
+
+  guardar() {
+
+    if(this.productividadForm.invalid){return}
 
     Swal.fire({
-      title: 'Espere',
-      text: 'Guardando información',
-      allowOutsideClick: false
-    });
-    Swal.showLoading();
+      title: `¿Desea agregar productividad?`,
+      text: `Se agregará la productividad al usuario`,
+      icon: 'question',
+      showCancelButton: true,
+      showConfirmButton: true,
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Aceptar'
+    }).then(resp => {
+      if(resp.value){
 
-    let peticion: Observable<any>;
+        Swal.fire({
+          title: 'Espere',
+          text: 'Guardando información',
+          allowOutsideClick: false
+        });
+        Swal.showLoading();
+    
+        const ruta = `${this.productividadForm.value.usuario}/${this.productividadForm.value.anio}`
+        var posicion = parseInt(this.productividadForm.value.mes);
+        const found = this.selectOptions.find(element => element.id === this.productividadForm.value.usuario)
+        this.productividad.usuario = found.nombre + ' ' + found.apellido
+        
+        this.AuthService.getProID(ruta).subscribe((next: ProductividadModel) => {
+          console.log(next);
+    
+          if (next != null) {
+            this.productividad = next
+            this.productividad.productividad.splice(posicion, 1, this.productividadForm.value.productividad)
+          } else {
+            let array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            array.splice(posicion, 1, this.productividadForm.value.productividad)
+            this.productividad.productividad = [...array]
+          }
+        }, error => {
+          console.log(error);
+        }, () => {
+          this.AuthService.agregarProductividad(this.productividad, ruta).subscribe(next => {
+            console.log(next);
+            Swal.fire({
+              title: `Productividad agregada`,
+              text: `Se le agrego a ${this.productividad.usuario} la productividad`,
+              icon: 'success'
+            })
+          }, error => {
+            console.log(error);
+          })
+        })
 
-    if(this.productividad.id){
-      peticion = this.AuthService.updatePro(this.productividad);
-    }else{
-      peticion = this.AuthService.addPro(this.productividad);
-    }
-
-    peticion.subscribe( resp => {
-      Swal.fire({
-        title: this.productividad.usuario,
-        text: 'Se asigno la productividad correctamente',
-        icon: 'success'
-      });
+      }
     })
-
   }
 
 }
