5dc16f2fc1942ed9b1796ce835ac36e13000d18a
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-09-20 15:13:24 -0500

    Agregar botón de archivo si es PDF o Word

--- src/app/components/vista-comunicado/vista-comunicado.component.ts
+++ src/app/components/vista-comunicado/vista-comunicado.component.ts
@@ -16,25 +16,36 @@ export class VistaComunicadoComponent implements OnInit {
   spinner: boolean = true;
   booleanTieneFile: boolean = false;
   id: any;
-  screenHeight:any;
-  screenWidth:any;
+  screenHeight: any;
+  screenWidth: any;
+  booleanEsPdf: boolean = false;
+  textoArchivo: string = '';
 
   constructor(private auth: AuthService, private route: ActivatedRoute, private fb: FormBuilder) {
     this.id = this.route.snapshot.paramMap.get('id');
     this.getScreenSize();
-    
+
     this.auth.getComunicado(this.id)
       .subscribe((resp: ComunicadoModel) => {
         this.spinner = false;
         this.comunicado = resp;
         this.comunicado.ids = this.id;
-        // console.log(resp);
+
+        if (this.comunicado.archivo) {
+          this.booleanEsPdf = this.comunicado.archivo.includes('pdf')
+          this.booleanTieneFile = true;
+          let pos = this.comunicado.archivo.lastIndexOf('%2F')
+          this.textoArchivo = this.comunicado.archivo.substring(pos + 3, pos + 18).replace('%20', ' ')
+        }
+
       }, error => {
         console.log(error);
-      }, () => {
-        // this.spinner = false;
-        // this.inicializar(this.comunicado)
-      });
+      }
+      // , () => {
+      //   // this.spinner = false;
+      //   // this.inicializar(this.comunicado)
+      // }
+      );
   }
 
   ngOnInit(): void {
@@ -42,28 +53,27 @@ export class VistaComunicadoComponent implements OnInit {
 
   @HostListener('window:resize', ['$event'])
   getScreenSize(event?) {
-        this.screenHeight = window.innerHeight;
-        this.screenWidth = window.innerWidth;
-        // console.log(this.screenHeight, this.screenWidth);
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    // console.log(this.screenHeight, this.screenWidth);
   }
 
 
-  inicializar(comunicado: ComunicadoModel) {
-    // console.log(comunicado);
+  // inicializar(comunicado: ComunicadoModel) {
+  //   // console.log(comunicado);
 
-    this.comunicadoForm = this.fb.group({
-      titulo: [{ value: comunicado.titulo, disabled: true }],
-      fecha: [{ value: comunicado.fecha, disabled: true }],
-      descripcion: [{ value: comunicado.descripcion, disabled: true }],
-      quien: [{ value: comunicado.quien, disabled: true }],
-      imagenes: [,],
-      archivo: [comunicado.archivo,],
-    })
-    this.spinner = false;
-    if (comunicado.archivo) {
-      this.booleanTieneFile = true
-    }
-
-  }
+  //   this.comunicadoForm = this.fb.group({
+  //     titulo: [{ value: comunicado.titulo, disabled: true }],
+  //     fecha: [{ value: comunicado.fecha, disabled: true }],
+  //     descripcion: [{ value: comunicado.descripcion, disabled: true }],
+  //     quien: [{ value: comunicado.quien, disabled: true }],
+  //     imagenes: [,],
+  //     archivo: [comunicado.archivo,],
+  //   })
+  //   this.spinner = false;
+  //   if (comunicado.archivo) {
+  //     this.booleanTieneFile = true
+  //   }
+  // }
 
 }
