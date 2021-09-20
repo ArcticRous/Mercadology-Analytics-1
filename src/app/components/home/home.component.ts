971f9d6e3ed6401b3abcb2d50690fe14742db328
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-09-20 09:38:50 -0500

    Cambiar diseño a cards con overflow-auto para mostrar los ultimos 5 comunicados realizados

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { ComunicadoModel } from "../../models/comunicado.model";
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-let comun;
 
 declare let $: any;
 
@@ -16,58 +12,27 @@ declare let $: any;
 export class HomeComponent implements OnInit {
 
   Comunicado: ComunicadoModel[] = [];
-
-  //  comunicado: ComunicadoModel = new ComunicadoModel;
-  catFilter = "General";
-  cargando = false;
-  durationInSeconds = 2;
+  cargando = true;
   rol: string;
-
-  displayedColumns: string[] = ['comunicado'];
-  dataSource: MatTableDataSource<ComunicadoModel>;
-
-  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
-  @ViewChild(MatSort, { static: false }) sort: MatSort;
 
   constructor(private Auth: AuthService) {
     Auth.leerToken();
   }
 
   ngOnInit(): void {
-
-    
     sessionStorage.removeItem('local');
     this.rol = sessionStorage.getItem('rol');
-    this.cargando = true;
+
     this.Auth.getComun("publico")
-      .subscribe(resp => {
-        comun = resp
+      .subscribe((resp) => {
         this.Comunicado = resp
+        this.Comunicado = this.Comunicado.slice(0,5)
+        // let result =  Object.keys(resp).slice(0,4).map(key => ({[key]:resp[key]}));
+      }, error => {
+        console.log(error);
+      }, () => {
         this.cargando = false;
-        this.dataSource = new MatTableDataSource(this.Comunicado);
-        this.paginator._intl.itemsPerPageLabel = "";
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-
       });
-  }//Termina ngOnInit
-
-  onSelect(event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
   }
 
 }
